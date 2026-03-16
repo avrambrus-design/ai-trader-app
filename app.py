e58d607d1f7b7f1181833e53b8ab997bdf81e178
avrambrus-design <avrambrus@gmail.com>
2026-03-16 19:37:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -31,12 +31,12 @@ if uploaded_file:
                 
                 with st.spinner('Анализирую рынок...'):
                     # Запрос к ИИ
-                    response = client.chat.completions.create(
+                    response = client.chat.completions.create
                         model="gpt-4o",
                         messages=
-                            }
-                        ]
-                    )
+                            
+                        
+                
                     st.subheader("📊 Вердикт:")
                     st.write(response.choices.message.content)
             except Exception as e:
